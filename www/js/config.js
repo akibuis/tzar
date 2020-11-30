78cea09494a340fe34e07e8ded17ff7d5bce627f
Akibu Ismail <akibuismail@yahoo.com>
2020-12-01 00:07:15 +0100

    update

--- www/js/config.js
+++ www/js/config.js
@@ -1,5 +1,5 @@
 var krms_config ={						
-	'ApiUrl':"http://food.connectzoneng.com/mobileappv2/api",    
+	'ApiUrl':"https://food.connectzoneng.com/mobileappv2/api",    
 	'AppTitle':"Tzar",
 	'ApiKey' : 'we45bbbxd569kl0r',	
 	'debug': false
